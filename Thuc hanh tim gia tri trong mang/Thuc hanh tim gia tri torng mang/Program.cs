e90d0f18369dcbfbbd0370b6bbe17194e3364def
Dattobias <152405844+datto2530@users.noreply.github.com>
2023-12-23 12:06:02 +0700

    daxong

---
+++ Thuc hanh tim gia tri trong mang/Thuc hanh tim gia tri torng mang/Program.cs
@@ -0,0 +1,30 @@
+﻿
+using System;
+
+namespace Mang
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] nameList = { "loc", "tri", "dat", "thang", "duc anh", "trung", "duc", "viet" };
+            Console.WriteLine("nhap ten ban: ");
+            string name = Console.ReadLine();
+            bool isTrue = false;
+            for (int i = 0; i <= nameList.Length-1 ; i++)
+            {
+                if (nameList[i].Equals(name))
+                {
+                    Console.WriteLine($"Co ten trong mang va vi tri {i+1}");
+                    isTrue = true;
+                    break;
+                }
+            }
+            if(isTrue=false)
+            {
+                Console.WriteLine("khong co ten");
+            }
+            Console.ReadKey();
+        }
+    }
+}
